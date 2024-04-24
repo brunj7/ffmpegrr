a5870c9207d2350a75ea4bec3a53439c045d9a25
Julien Brun <brun.julien@gmail.com>
2024-04-23 22:41:08 -0700

    first function to concatenate video

---
+++ R/concat_clips.R
@@ -0,0 +1,15 @@
+
+concat_clips <- function(folder, no_audio=TRUE) {
+  # get the video clips
+  video_clips <- list.files(path = folder, pattern = "MP4", full.names = TRUE)
+  # create the clip list
+  capture.output(
+    for (f in video_clips) {
+      cat(noquote(sprintf("file '%s'\n", f)))
+      },
+    file = file.path(folder, "clips_list.txt")
+  )
+  system2(
+    sprintf("ffmpeg  -f concat -safe 0 -i %s/clips_list.txt -an -c copy %s/output_test_noaudio.mp4", "/Users/julien/Data/Caves/Ped\ 1/09_16_23\ 1108\ baseline","/Users/julien/Data/Caves/Ped\ 1/09_16_23\ 1108\ baseline")
+          ) # does not keep the space escape
+}
